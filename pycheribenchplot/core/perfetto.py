d5e2aed1be8edf5307db19679149e04aa5a08000
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2021-10-15 13:07:35 +0100

    Cache perfetto trace processor instances across datasets.
    Reuse loaded traces to avoid wasting time to reload the same file
    multiple times.

--- pycheribenchplot/core/perfetto.py
+++ pycheribenchplot/core/perfetto.py
@@ -1,3 +1,4 @@
+import logging
 from abc import abstractmethod
 from pathlib import Path
 
@@ -7,15 +8,53 @@ from perfetto.trace_processor import TraceProcessor
 from .dataset import DataSetContainer
 
 
+class _TraceProcessorCache:
+    """
+    Cache trace processor instances for later access so that we only load traces once.
+    """
+    instance = None
+
+    @classmethod
+    def get_instance(cls, manager_config: "BenchmarkManagerConfig"):
+        if cls.instance is None:
+            cls.instance = _TraceProcessorCache(manager_config)
+        return cls.instance
+
+    def __init__(self, manager_config: "BenchmarkManagerConfig"):
+        self._manager_config = manager_config
+        self._instances = {}
+        self.logger = logging.getLogger("perfetto-trace-processor-cache")
+
+    def _trace_processor_path(self):
+        return self._manager_config.perfetto_path / "trace_processor_shell"
+
+    def get_trace_processor(self, trace_path: Path) -> TraceProcessor:
+        if trace_path in self._instances:
+            return self._instances[trace_path]
+        self.logger.debug("New trace processor for %s", trace_path)
+        processor = TraceProcessor(bin_path=self._trace_processor_path(), file_path=trace_path)
+        self._instances[trace_path] = processor
+        return processor
+
+    def __del__(self):
+        for tp in self._instances.values():
+            tp.close()
+
+
 class PerfettoDataSetContainer(DataSetContainer):
     # Map columns in the SQL expression
     key_to_column_map = {}
 
     def __init__(self, benchmark: "BenchmarkBase", dset_key: str):
         super().__init__(benchmark, dset_key)
+        self._tp_cache = _TraceProcessorCache.get_instance(benchmark.manager_config)
 
-    def _trace_processor_path(self):
-        return self.benchmark.manager_config.perfetto_path / "trace_processor_shell"
+    def _integrity_check(self, tp: TraceProcessor):
+        result = tp.query("SELECT * FROM stats WHERE name = 'traced_buf_trace_writer_packet_loss'")
+        rows = list(result)
+        assert len(rows) == 1, "Query for stats.traced_buf_trace_writer_packet_loss failed"
+        if rows[0].value != 0:
+            self.logger.error("!!!Perfetto packet loss detected!!!")
 
     def _get_sql_table(self):
         """Get the table to select from"""
@@ -45,6 +84,7 @@ class PerfettoDataSetContainer(DataSetContainer):
         ...
 
     def _extract_events(self, tp: TraceProcessor):
+        self._integrity_check(tp)
         query_str = self._get_sql_expr()
         result = tp.query(query_str)
         # XXX-AM: This is unreasonably slow, build the dataframe manually for now
@@ -55,8 +95,5 @@ class PerfettoDataSetContainer(DataSetContainer):
         self.df = pd.concat([self.df, df])
 
     def load(self, path: Path):
-        processor = TraceProcessor(bin_path=self._trace_processor_path(), file_path=path)
-        try:
-            self._extract_events(processor)
-        finally:
-            processor.close()
+        processor = self._tp_cache.get_trace_processor(path)
+        self._extract_events(processor)
