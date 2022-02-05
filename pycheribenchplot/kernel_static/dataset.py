11566912bbcb778e5c9c3da135d27c599b418bd1
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2022-02-05 00:57:08 +0000

    Add multiple plots for kernel structure static analysis

--- pycheribenchplot/kernel_static/dataset.py
+++ pycheribenchplot/kernel_static/dataset.py
@@ -1,31 +1,31 @@
 import asyncio as aio
+from enum import Enum
 from shutil import copyfileobj
 
 import numpy as np
 import pandas as pd
 
 from ..core.csv import CSVDataSetContainer
-from ..core.dataset import DatasetArtefact, DatasetName, Field
+from ..core.dataset import (DatasetArtefact, DatasetName, DatasetProcessingError, Field, align_multi_index_levels)
 from ..core.instance import InstanceKernelABI
 
 
-class SubobjectBoundsStatsDataset(CSVDataSetContainer):
-    """
-    Extract subobject bounds statistics from cheribuild targets.
-    These can be used to inspect statically the distribution of
-    subobject bounds.
-    """
-    fields = [
-        Field("alignment_bits", dtype=float),
-        Field("size", dtype=float),
-        Field.str_field("kind"),
-        Field.str_field("source_loc"),
-        Field.str_field("compiler_pass"),
-        Field.str_field("details"),
-    ]
+class SetboundsKind(Enum):
+    STACK = "s"
+    HEAP = "h"
+    SUBOBJECT = "o"
+    GLOBAL = "g"
+    CODE = "c"
+    UNKNOWN = "?"
+
+    def __lt__(self, o):
+        return self.value < o.value
+
+    def __le__(self, o):
+        return self.value <= o.value
 
 
-class KernelSubobjectBoundsDataset(SubobjectBoundsStatsDataset):
+class KernelSubobjectBoundsDataset(CSVDataSetContainer):
     """
     Extract subobject bounds from kernel builds.
     For the current benchmark instance, we grab the instance configuration and determine
@@ -34,17 +34,40 @@ class KernelSubobjectBoundsDataset(SubobjectBoundsStatsDataset):
     dataset_config_name = DatasetName.KERNEL_CSETBOUNDS_STATS
     dataset_source_id = DatasetArtefact.KERNEL_CSETBOUNDS_STATS
 
-    def _output_files_cheribuild(self):
+    fields = [
+        Field("alignment_bits", dtype=float),
+        Field("size", dtype=float),
+        Field.str_field("src_module"),
+        Field("kind", dtype="object", importfn=lambda v: SetboundsKind(v)),
+        Field.str_field("source_loc"),
+        Field.str_field("compiler_pass"),
+        Field.str_field("details"),
+    ]
+
+    def _kernel_build_path(self):
         mconfig = self.benchmark.manager_config
         iconfig = self.benchmark.instance_config
         build_base = mconfig.build_path / f"cheribsd-{iconfig.cheri_target}-build"
         path_match = list(build_base.glob(f"**/sys/{iconfig.kernel}"))
         if len(path_match) == 0:
             self.logger.error("No kernel build directory for %s in %s", iconfig.kernel, build_base)
-            return None
+            raise DatasetProcessingError("Missing kernel build directory")
         assert len(path_match) == 1
-        return list(path_match[0].glob("**/kernel-subobject-bounds-stats.csv"))
-        # return path_match[0] / "kernel-subobject-bounds-stats.csv"
+        return path_match[0]
+
+    def _llvm_output_kernel(self):
+        build_path = self._kernel_build_path()
+        path = build_path / "kernel-subobject-bounds-stats.csv"
+        if not path.exists():
+            self.logger.error(
+                "Missing csetbounds stats for %s, you may need to" +
+                " build the kernels with --collect-csetbounds-stats", self.benchmark.instance_config.kernel)
+            raise DatasetProcessingError("Missing kernel csetbounds stats")
+        return path
+
+    def _llvm_output_modules(self):
+        build_path = self._kernel_build_path()
+        return list(build_path.glob("modules/**/kernel-subobject-bounds-stats.csv"))
 
     def output_file(self):
         base = super().output_file()
@@ -52,20 +75,16 @@ class KernelSubobjectBoundsDataset(SubobjectBoundsStatsDataset):
 
     async def after_extract_results(self):
         await super().after_extract_results()
-        cheribuild_out = self._output_files_cheribuild()
         iconfig = self.benchmark.instance_config
         if iconfig.kernelabi == InstanceKernelABI.PURECAP:
-            if not cheribuild_out:
-                self.logger.error(
-                    "Missing csetbounds stats for %s, you may need to" +
-                    " build the kernels with --collect-csetbounds-stats", iconfig.kernel)
-                return
             with open(self.output_file(), "w+") as outfd:
-                header = True
-                for stats_file in cheribuild_out:
+                df = pd.read_csv(self._llvm_output_kernel())
+                df["src_module"] = "kernel"
+                df.to_csv(outfd)
+                for stats_file in self._llvm_output_modules():
                     df = pd.read_csv(stats_file)
-                    df.to_csv(outfd, header=header)
-                    header = False
+                    df["src_module"] = stats_file.parent.name
+                    df.to_csv(outfd, header=False)
         else:
             # Just make an empty file for load() to find it
             pd.DataFrame({}, columns=self.input_all_columns()).to_csv(self.output_file())
@@ -87,11 +106,12 @@ class KernelStructSizeDataset(CSVDataSetContainer):
     dataset_source_id = DatasetArtefact.KERNEL_STRUCT_STATS
     fields = [
         Field.index_field("name", dtype=str),
-        Field.index_field("size", dtype=int),
-        Field("is_anon", dtype=bool),
+        Field.index_field("src_file", dtype=str),
+        Field.index_field("src_line", dtype=int),
+        Field.index_field("is_anon", dtype=np.bool),
+        Field.data_field("size", dtype=int),
         Field.str_field("from_path"),
-        Field.str_field("src_file"),
-        Field("src_line", dtype=int),
+        Field.str_field("desc", isderived=True),
     ]
 
     def full_kernel_path(self):
@@ -134,6 +154,38 @@ class KernelStructSizeDataset(CSVDataSetContainer):
     def load(self):
         df = self._load_csv(self.output_file())
         self._append_df(df)
+
+    def post_merge(self):
+        super().post_merge()
+        # compute patched anon names in the "desc" column
+        df_index = self.merged_df.index.to_frame()
+        name = df_index["name"]
+        # bool becomes object when moved to the index for some reason...
+        anon = df_index["is_anon"].astype(np.bool)
+        tmp = name.mask(anon, "anon")
+        suffix_file = df_index["src_file"].where(anon, "")
+        suffix_line = df_index["src_line"].astype(str).where(anon, "")
+        self.merged_df["desc"] = tmp.str.cat([suffix_file, suffix_line], sep=":").where(anon, name)
+
+        # Align and compute deltas
+        new_df = align_multi_index_levels(self.merged_df, ["name", "src_file", "src_line", "is_anon"],
+                                          fill_value=np.nan)
+        new_df = self._add_delta_columns(new_df)
+        self.merged_df = self._compute_delta_by_dataset(new_df)
+
+    def _get_aggregation_strategy(self):
+        agg = super()._get_aggregation_strategy()
+        metric_cols = self.merged_df.columns.get_level_values("metric")
+        metric_level_idx = self.merged_df.columns.names.index("metric")
+        match = metric_cols.isin(agg.keys())
+        mapped_agg = {c: agg[c[metric_level_idx]] for c in self.merged_df.columns[match]}
+        # agg = {(c, "sample"): v for c, v in agg.items()}
+        return mapped_agg
+
+    def aggregate(self):
+        super().aggregate()
+        grouped = self.merged_df.groupby(["__dataset_id"])
+        self.agg_df = self._compute_aggregations(grouped)
 
 
 class KernelStructFragDataset:
--- pycheribenchplot/kernel_static/plot.py
+++ pycheribenchplot/kernel_static/plot.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
 
-from ..core.dataset import (DatasetName, check_multi_index_aligned, pivot_multi_index_level, subset_xs)
+from ..core.dataset import (DatasetName, index_where, pivot_multi_index_level, stacked_histogram)
+from ..core.excel import SpreadsheetSurface
 from ..core.matplotlib import MatplotlibSurface
-from ..core.plot import (BenchmarkPlot, BenchmarkSubPlot, CellData, ColorMap, HistPlotDataView, Scale, Surface)
+from ..core.plot import (AALineDataView, BarPlotDataView, BenchmarkPlot, BenchmarkSubPlot, ColorMap, HistPlotDataView,
+                         Scale, TableDataView)
 
 
-class KernelBoundsDistribution(BenchmarkSubPlot):
+class SetBoundsDistribution(BenchmarkSubPlot):
     """
     Draw kernel (static) csetbounds distribution by size of enforced bounds.
     """
@@ -21,15 +23,27 @@ class KernelBoundsDistribution(BenchmarkSubPlot):
         self.bounds_stats = self.get_dataset(DatasetName.KERNEL_CSETBOUNDS_STATS)
         assert self.bounds_stats is not None, "Can not find required dataset"
 
-    def get_cell_title(self):
-        return "Kernel bounds histogram"
+    def get_stats_df(self):
+        raise NotImplementedError("Must override")
 
-    def generate(self, surface: Surface, cell: CellData):
+    def get_legend_map(self):
+        df = self.get_stats_df()
+        datasets = df.index.get_level_values("__dataset_id").unique()
+        group = self.benchmark.get_benchmark_group()
+        # Only use the datasets in the dataframe
+        labels = {uuid: str(b.instance_config.name) for uuid, b in group.items() if uuid in datasets}
+        if self.benchmark.uuid in labels:
+            labels[self.benchmark.uuid] += "(*)"
+        return ColorMap.from_keys(labels.keys(), mapname="Paired", labels=labels.values())
+
+
+class SetBoundsSimpleDistribution(SetBoundsDistribution):
+    def generate(self, surface, cell):
         """
         Generate interleaved histograms with one set of bars for each dataset, so
         that we have side-by-side comparison of the buckets.
         """
-        df = self.bounds_stats.agg_df
+        df = self.get_stats_df()
         nviews = len(df.index.get_level_values("__dataset_id").unique())
         # Determine buckets we are going to use
         min_size = max(df["size"].min(), 1)
@@ -38,29 +52,345 @@ class KernelBoundsDistribution(BenchmarkSubPlot):
         buckets = [2**i for i in log_buckets]
 
         # Build histograms for each dataset
-        view = HistPlotDataView("hist",
-                                df,
-                                x="size",
-                                x_scale=Scale("log", base=2),
-                                buckets=buckets,
-                                bucket_group="__dataset_id")
+        view = HistPlotDataView("hist", df, x="size", buckets=buckets, bucket_group="__dataset_id")
+        cell.legend_map = self.get_legend_map()
+        cell.x_ticks = buckets
+        cell.x_config.label = "Bounds size (bytes)"
+        cell.x_config.scale = Scale("log", base=2)
+        cell.yleft_config.label = "#csetbounds"
         cell.add_view(view)
 
 
-class KernelSubobjectPlot(BenchmarkPlot):
+class KernelBoundsDistribution(SetBoundsSimpleDistribution):
+    def get_cell_title(self):
+        return "Kernel bounds histogram"
+
+    def get_stats_df(self):
+        df = self.bounds_stats.merged_df
+        return df[df["src_module"] == "kernel"]
+
+
+class ModulesBoundsDistribution(SetBoundsSimpleDistribution):
+    def get_cell_title(self):
+        return "Modules bounds histogram"
+
+    def get_stats_df(self):
+        df = self.bounds_stats.merged_df
+        return df[df["src_module"] != "kernel"]
+
+
+class KernelBoundsDistributionByKind(SetBoundsDistribution):
+    def get_cell_title(self):
+        return "Kernel bounds by kind"
+
+    def get_stats_df(self):
+        df = self.bounds_stats.merged_df
+        return df[df["src_module"] == "kernel"]
+
+    def get_kind_legend_map(self):
+        df = self.get_stats_df()
+        kinds = df["kind"].unique()
+        labels = {k: k.name for k in kinds}
+        return ColorMap.from_keys(labels.keys(), mapname="Paired", labels=labels.values())
+
+    def generate(self, surface, cell):
+        """
+        Generate interleaved and stacked histograms.
+        Horizontal bars are created for each datasets, stacked bars are generated
+        for each setbounds kind.
+        """
+        df = self.get_stats_df()
+        nviews = len(df.index.get_level_values("__dataset_id").unique())
+        # Determine buckets we are going to use
+        min_size = max(df["size"].min(), 1)
+        max_size = max(df["size"].max(), 1)
+        log_buckets = range(int(np.log2(min_size)), int(np.log2(max_size)) + 1)
+        buckets = [2**i for i in log_buckets]
+
+        # Build the stacked histogram dataframe
+        hist_df = stacked_histogram(df, group="__dataset_id", stack="kind", data_col="size", bins=buckets)
+
+        view = BarPlotDataView("bar",
+                               hist_df,
+                               x="bin_start",
+                               yleft="count",
+                               bar_group="__dataset_id",
+                               stack_group="kind")
+
+        # Build histograms for each dataset
+        cell.legend_map = self.get_kind_legend_map()
+        cell.legend_level = "kind"
+        cell.x_config.ticks = buckets
+        cell.x_config.label = "Bounds size (bytes)"
+        cell.x_config.scale = Scale("log", base=2)
+        cell.yleft_config.label = "#csetbounds"
+        cell.add_view(view)
+
+
+class KernelStructStatsPlot(BenchmarkSubPlot):
+    """
+    Base class for kernel struct size plots
+    """
+    @classmethod
+    def get_required_datasets(cls):
+        dsets = super().get_required_datasets()
+        dsets += [DatasetName.KERNEL_STRUCT_STATS]
+        return dsets
+
+    def __init__(self, plot):
+        super().__init__(plot)
+        self.struct_stat = self.get_dataset(DatasetName.KERNEL_STRUCT_STATS)
+        assert self.struct_stat is not None, "Can not find required dataset"
+
+    def get_legend_map(self):
+        legend = {uuid: str(bench.instance_config.name) for uuid, bench in self.benchmark.merged_benchmarks.items()}
+        legend[self.benchmark.uuid] = f"{self.benchmark.instance_config.name}(*)"
+        legend_map = ColorMap.from_keys(legend.keys(), mapname="Paired", labels=legend.values())
+        return legend_map
+
+
+class KernelStructSizeHist(KernelStructStatsPlot):
+    def get_median_line_legend(self):
+        legend = {
+            uuid: "median " + str(bench.instance_config.name)
+            for uuid, bench in self.benchmark.merged_benchmarks.items()
+        }
+        legend[self.benchmark.uuid] = f"median {self.benchmark.instance_config.name}(*)"
+        legend_map = ColorMap.from_keys(legend.keys(), mapname="Greys", labels=legend.values(), color_range=(0.5, 1))
+        return legend_map
+
+    def get_hist_column(self):
+        raise NotImplementedError("Must override")
+
+    def get_df(self):
+        return self.struct_stat.merged_df
+
+    def get_agg_df(self):
+        return self.struct_stat.agg_df
+
+    def get_df_selector(self):
+        return None
+
+    def build_buckets(self, df):
+        # Determine bucket sizes
+        hcol = self.get_hist_column()
+        min_size = df[hcol].min()
+        max_size = df[hcol].max()
+        if np.sign(min_size) == np.sign(max_size) or min_size == 0 or max_size == 0:
+            interval = sorted(np.abs((min_size, max_size)))
+            log_min, log_max = np.ceil(np.log2(np.maximum(interval, 1))).astype(int)
+            buckets = [2**i for i in range(log_min, log_max + 1)]
+            if min_size < 0:
+                buckets = list(-1 * np.array(buckets))
+        else:
+            neg_min = abs(min_size)
+            neg_log = np.ceil(np.log2(neg_min)).astype(int)
+            pos_log = np.ceil(np.log2(max_size)).astype(int)
+            neg_buckets = [-2**i for i in range(0, neg_log + 1)]
+            pos_buckets = [2**i for i in range(0, pos_log + 1)]
+            buckets = neg_buckets + pos_buckets
+        return sorted(buckets)
+
+    def generate(self, surface, cell):
+        df = self.get_df()
+        agg_df = self.get_agg_df()
+        hcol = self.get_hist_column()
+        buckets = self.build_buckets(df)
+
+        # Build histogram
+        view = HistPlotDataView("hist", df, x=hcol, buckets=buckets, bucket_group="__dataset_id")
+        cell.add_view(view)
+
+        # Add help lines for the median struct size
+        line_cols = [hcol + ("median", )]
+        view = AALineDataView("axline", agg_df, vertical=line_cols)
+        view.style.line_style = "dashed"
+        view.legend_map = self.get_median_line_legend()
+        view.legend_level = "__dataset_id"
+        cell.add_view(view)
+
+        cell.x_config.label = "Size (bytes)"
+        cell.x_config.scale = Scale("symlog", base=2, lintresh=1, linscale=0.25)
+        cell.x_config.ticks = buckets
+        cell.x_config.limits = (min(buckets), max(buckets))
+        cell.yleft_config.label = "# structs"
+        cell.legend_map = self.get_legend_map()
+        cell.legend_level = "__dataset_id"
+
+
+class KernelStructSizeDistribution(KernelStructSizeHist):
+    """
+    Draw kernel structure size shift in distribution
+    """
+    def get_cell_title(self):
+        return "Kernel structure size distribution"
+
+    def get_hist_column(self):
+        return ("size", "sample")
+
+
+class KernelStructSizeOverhead(KernelStructSizeHist):
+    def get_cell_title(self):
+        return "Kernel structure size overhead"
+
+    def get_hist_column(self):
+        return ("size", "delta_baseline")
+
+    def get_df(self):
+        df = super().get_df()
+        # take the non-baseline delta values only
+        baseline = df.index.get_level_values("__dataset_id") == self.benchmark.uuid
+        return df[~baseline]
+
+    def get_agg_df(self):
+        df = super().get_agg_df()
+        # take the non-baseline delta values only
+        baseline = df.index.get_level_values("__dataset_id") == self.benchmark.uuid
+        return df[~baseline]
+
+    def generate(self, surface, cell):
+        super().generate(surface, cell)
+        cell.x_config.label = "size delta (bytes)"
+        cell.yleft_config.scale = Scale("log", base=10)
+
+
+class KernelStructSizeRelOverhead(KernelStructSizeHist):
+    def get_cell_title(self):
+        return "Kernel structure size overhead"
+
+    def get_hist_column(self):
+        return ("size", "norm_delta_baseline")
+
+    def get_df(self):
+        df = super().get_df()
+        # take the non-baseline delta values only
+        baseline = df.index.get_level_values("__dataset_id") == self.benchmark.uuid
+        df = df[~baseline].copy()
+        df[self.get_hist_column()] *= 100
+        return df
+
+    def get_agg_df(self):
+        df = super().get_agg_df()
+        # take the non-baseline delta values only
+        baseline = df.index.get_level_values("__dataset_id") == self.benchmark.uuid
+        df = df[~baseline].copy()
+        df[self.get_hist_column() + ("median", )] *= 100
+        return df
+
+    def build_buckets(self, df):
+        min_val = df[self.get_hist_column()].min()
+        max_val = df[self.get_hist_column()].max()
+        min_val = int(np.floor(min_val / 10) * 10)
+        max_val = int(np.ceil(max_val / 10) * 10)
+        buckets = range(min_val, max_val + 10, 10)
+        return buckets
+
+    def generate(self, surface, cell):
+        super().generate(surface, cell)
+        cell.x_config.label = "% size delta"
+        cell.x_config.scale = Scale("linear")
+        cell.yleft_config.scale = Scale("log", base=10)
+
+
+class KernelStructSizeLargeOverhead(KernelStructStatsPlot):
+    """
+    Show the structures responsible for the largest overhead
+    """
+    def get_col(self):
+        raise NotImplementedError("Must override")
+
+    def generate(self, surface, cell):
+        df = self.struct_stat.merged_df
+        col = self.get_col()
+        # Get range high 10%
+        high_thresh = df[col].quantile(0.9)
+        cond = (df[col] >= high_thresh)
+        high_df = df[cond].droplevel("__iteration")
+        assert high_df.index.is_unique, "Non unique index?"
+
+        # Fixup the name index for the table for anon structures
+        desc = high_df[("desc", "sample")]
+        # For some reason boolean index levels end up being obj instead of bool...
+        anon = high_df.index.get_level_values("is_anon").values.astype(bool)
+        high_df = index_where(high_df, "name", ~anon, desc)
+        assert not high_df.index.get_level_values("name").isna().any(), "NaN names"
+
+        legend_map = self.get_legend_map()
+        # Currently only use the legend_map to map labels to __dataset_id
+        show_df = legend_map.map_labels_to_level(high_df, "__dataset_id", axis=0)
+        show_df = pivot_multi_index_level(show_df, "__dataset_id")
+        # sort by highest delta
+        sort_cols_sel = None
+        for i, level_value in enumerate(col):
+            sel = (show_df.columns.get_level_values(i) == level_value)
+            if sort_cols_sel is None:
+                sort_cols_sel = sel
+            else:
+                sort_cols_sel = sort_cols_sel & sel
+        sort_cols = show_df.columns[sort_cols_sel].to_list()
+        show_df.sort_values(by=sort_cols, ascending=False, inplace=True)
+
+        show_cols = show_df.columns.get_level_values("metric").isin(self.struct_stat.data_columns())
+        col_idx = show_df.columns[show_cols]
+        view = TableDataView("table", show_df, columns=col_idx)
+        cell.add_view(view)
+
+
+class KernelStructSizeLargeRelOverhead(KernelStructSizeLargeOverhead):
+    def get_cell_title(self):
+        return "Large % overhead (>90th percentile)"
+
+    def get_col(self):
+        return ("size", "norm_delta_baseline")
+
+
+class KernelStructSizeLargeAbsOverhead(KernelStructSizeLargeOverhead):
+    def get_cell_title(self):
+        return "Large absolute overhead (>90th percentile)"
+
+    def get_col(self):
+        return ("size", "delta_baseline")
+
+
+class KernelStaticInfoPlot(BenchmarkPlot):
     """
     Report subobject bounds statistics for the kernel
     """
 
     subplots = [
         KernelBoundsDistribution,
+        ModulesBoundsDistribution,
+        KernelBoundsDistributionByKind,
+        KernelStructSizeDistribution,
+        KernelStructSizeOverhead,
+        KernelStructSizeRelOverhead,
     ]
 
     def __init__(self, benchmark):
         super().__init__(benchmark, [MatplotlibSurface()])
 
     def get_plot_name(self):
-        return "Kernel subobject bounds stats"
+        return "Kernel compile-time stats"
 
     def get_plot_file(self):
-        return self.benchmark.manager.session_output_path / "kernel-subobject-bounds"
+        return self.benchmark.manager.session_output_path / "kernel-static-stats"
+
+
+class KernelStaticInfoTables(BenchmarkPlot):
+    """
+    Report subobject bounds statistics for the kernel
+    """
+
+    subplots = [
+        KernelStructSizeLargeRelOverhead,
+        KernelStructSizeLargeAbsOverhead,
+    ]
+
+    def __init__(self, benchmark):
+        super().__init__(benchmark, [SpreadsheetSurface()])
+
+    def get_plot_name(self):
+        return "Kernel compile-time detailed stats"
+
+    def get_plot_file(self):
+        return self.benchmark.manager.session_output_path / "kernel-static-tables"
