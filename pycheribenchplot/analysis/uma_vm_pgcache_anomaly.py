62bce392945c967c1e1a7c174a0e672221bfa186
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2022-03-15 13:08:09 +0000

    Add arrow plot for UMA bucket zone deltas.

--- pycheribenchplot/analysis/uma_vm_pgcache_anomaly.py
+++ pycheribenchplot/analysis/uma_vm_pgcache_anomaly.py
@@ -3,9 +3,9 @@ import itertools as it
 import numpy as np
 import pandas as pd
 
-from ..core.dataset import DatasetName, assign_sorted_coord
-from ..core.plot import (AALineDataView, BarPlotDataView, BenchmarkPlot, BenchmarkSubPlot, CellData, HistPlotDataView,
-                         LegendInfo, Mosaic, Scale)
+from ..core.dataset import (DatasetName, assign_sorted_coord, broadcast_xs, subset_xs)
+from ..core.plot import (AALineDataView, ArrowPlotDataView, BarPlotDataView, BenchmarkPlot, BenchmarkSubPlot, CellData,
+                         HistPlotDataView, LegendInfo, Mosaic, Scale)
 from ..vmstat.plot import VMStatUMAMetricHist
 
 
@@ -79,6 +79,54 @@ class UMABucketAllocAnomaly(BenchmarkPlot):
         return self.benchmark.manager.plot_output_path / "uma-bucket-vm-pgcache-anomaly"
 
 
+class UMABucketAffinityBase(BenchmarkSubPlot):
+    """
+    Base class for collecting information about UMA bucket zones
+    """
+    def _get_ptr_size(self, dsid):
+        if dsid in self.benchmark.merged_benchmarks:
+            return self.benchmark.merged_benchmarks[dsid].instance_config.kernel_pointer_size
+        else:
+            return self.benchmark.instance_config.kernel_pointer_size
+
+    def get_uma_df(self):
+        """
+        Return the UMA info dataset with an additional column to map the bucket size to the
+        corresponding bucket zone
+
+        XXX should we do this in the dataset directly?
+        """
+        size_col = ("bucket_size", "-", "sample")
+        df = self.get_dataset(DatasetName.VMSTAT_UMA_INFO).agg_df.copy()
+        struct_stats_df = self.get_dataset(DatasetName.KERNEL_STRUCT_STATS).merged_df
+        # Note that UMA bucket sizes larger than 16 use some of the space for the bucket header.
+        # so we compute the actual size of the header and subtract it where needed.
+        s_uma_bucket = struct_stats_df.xs("uma_bucket", level="name")
+        datasets = df.index.get_level_values("dataset_id").unique().to_frame()["dataset_id"]
+        if len(s_uma_bucket) != len(datasets):
+            self.logger.error("kernel DWARF stats for struct uma_bucket do not agree with vmstat data. Skipping plot.")
+            return
+        ptr_size = pd.Series([self._get_ptr_size(dsid) for dsid in datasets], index=datasets)
+        hdr_size = s_uma_bucket.groupby("dataset_id").first()[("size", "sample")]
+        hdr_items = (hdr_size / ptr_size).apply(np.ceil)
+        # Build a mapping df with the base bucket sizes for each dataset
+        # we then subtract the header items and use this to fixup the bucket_zone column
+        bucket_size_base = pd.Series([0] + [2**i for i in range(1, 9)], name="bucket_zone")
+        zone_mapping = pd.merge(datasets, bucket_size_base, how="cross").set_index("dataset_id")
+        size_no_hdr = zone_mapping["bucket_zone"] - hdr_items
+        zone_mapping["bucket_zone_size"] = zone_mapping["bucket_zone"].mask(zone_mapping["bucket_zone"] > 16,
+                                                                            size_no_hdr)
+        tmp_df = df.copy()
+        tmp_df.columns = df.columns.to_flat_index()
+        tmp_df = tmp_df.join(zone_mapping, on="dataset_id")
+        high_cond = tmp_df[size_col] <= tmp_df["bucket_zone_size"]
+        low_cond = tmp_df[size_col] > tmp_df["bucket_zone_size"].shift(1, fill_value=0)
+        tmp_df = tmp_df[high_cond & low_cond]
+        # Note bucket_zone will be nan for zones that have buckets disabled
+        df["bucket_zone"] = tmp_df["bucket_zone"]
+        return df
+
+
 class UMABucketAffinityHist(BenchmarkSubPlot):
     """
     Show histogram of number of zones using each bucket zone. This helps understanding
@@ -135,6 +183,58 @@ class UMABucketAffinityHist(BenchmarkSubPlot):
         cell.add_view(view)
 
 
+class UMABucketAffinityDelta(UMABucketAffinityBase):
+    """
+    Show the change in bucket zone by displaying an arrow that shows the change.
+    The X axis is the bucket size, the Y axis is the zone name.
+    """
+    def get_cell_title(self):
+        return "Bucket affinity delta"
+
+    def get_mosaic_extent(self):
+        return (5, 1)
+
+    def get_legend_info(self):
+        legend = self.build_legend_by_dataset()
+        legend = legend.assign_colors_hsv("dataset_id", h=(0.2, 1), s=(0.7, 0.7), v=(0.6, 0.9))
+        return legend
+
+    def generate(self, fm, cell):
+        df = self.get_uma_df()
+        # Drop all entries where the bucket_zone does not change or where there is no bucket zone
+        index_complement = df.index.names.difference(["dataset_id"])
+        changed = df.groupby(index_complement).nunique()["bucket_zone"] > 1
+        view_df = subset_xs(df, changed)
+        assert not view_df["bucket_zone"].isna().any()
+
+        base = view_df.xs(self.benchmark.uuid, level="dataset_id")
+        sorted_base = base.sort_values("bucket_zone")
+        sorted_base["y"] = range(len(sorted_base))
+        view_df["y"] = broadcast_xs(view_df, sorted_base["y"])
+
+        view = ArrowPlotDataView(view_df,
+                                 x="bucket_zone",
+                                 y="y",
+                                 group_by=["dataset_id"],
+                                 base_group=[self.benchmark.uuid])
+        view.legend_info = self.get_legend_info()
+        view.legend_level = ["dataset_id"]
+        cell.add_view(view)
+
+        x_ticks = view_df["bucket_zone"].unique()
+        y_ticks = view_df["y"].unique()
+        cell.x_config.label = "Bucket zone"
+        cell.x_config.ticks = x_ticks
+        cell.x_config.tick_labels = [f"{x:.0f}" for x in x_ticks]
+        cell.x_config.scale = Scale("log", base=2)
+        cell.yleft_config.padding = 0.02
+        cell.yleft_config.limits = (y_ticks.min(), y_ticks.max())
+        cell.yleft_config.origin_line = False
+        cell.yleft_config.label = "Zone"
+        cell.yleft_config.ticks = y_ticks
+        cell.yleft_config.tick_labels = view_df.index.get_level_values("name").unique()
+
+
 class UMABucketRefillEff(BenchmarkSubPlot):
     """
     Show the number of requests made to each bucket zone. This is used to report pressure on each bucket
@@ -174,7 +274,7 @@ class UMABucketAnalysis(BenchmarkPlot):
     """
     Collect bucket-related data for the pgcache anomaly analysis
     """
-    subplots = [UMABucketAffinityHist, UMABucketRefillEff]
+    subplots = [UMABucketAffinityHist, UMABucketRefillEff, UMABucketAffinityDelta]
 
     def get_plot_name(self):
         return "UMA Bucket analysis"
