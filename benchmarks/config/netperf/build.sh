6402987c442e51582c270a5225964d3bfbd6f543
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2022-07-29 15:38:14 +0100

    Add netperf build command to sample build script.

--- benchmarks/config/netperf/build.sh
+++ benchmarks/config/netperf/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+${CHERIBUILD} --skip-update netperf-riscv64-purecap --netperf-riscv64-purecap/configure-options='--enable-pmc-profile'
 ${CHERIBUILD} --skip-update cheribsd-riscv64-purecap
 ${CHERIBUILD} --skip-update disk-image-mfs-root-riscv64-purecap
 ${CHERIBUILD} --skip-update --skip-buildworld cheribsd-mfs-root-kernel-riscv64-purecap --cheribsd/build-bench-kernels --cheribsd/build-fpga-kernels --cheribsd/extra-kernel-config "CHERI-GFE-BUCKET-ADJUST CHERI-PURECAP-GFE-BUCKET-ADJUST CHERI-PURECAP-NOSUBOBJ-GFE-NODEBUG"
