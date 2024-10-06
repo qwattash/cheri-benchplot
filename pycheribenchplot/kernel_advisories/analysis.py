e0d196fc15c9a56612f49d17607698e365d6a339
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2024-10-06 18:27:12 +0100

    Port advisories table information generators to new data format.

--- pycheribenchplot/kernel_advisories/analysis.py
+++ pycheribenchplot/kernel_advisories/analysis.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from datetime import date
 
 import polars as pl
@@ -5,11 +6,23 @@ import seaborn as sns
 from matplotlib.ticker import AutoMinorLocator
 
 from ..core.analysis import AnalysisTask
-from ..core.artefact import DataFrameTarget, Target, make_dataframe_loader
-from ..core.config import Config, ConfigPath, validate_file_exists
+from ..core.artefact import DataFrameTarget, Target
+from ..core.config import (Config, ConfigPath, config_field, validate_file_exists)
 from ..core.plot import PlotTarget, PlotTask, new_figure
 from ..core.task import SessionDataGenTask, dependency, output
 from .ingest import IngestAdvisoriesTask
+
+DEFAULT_EVENTS = {
+    date(2021, 4, 13): "Added KASAN kernel option",
+    date(2017, 10, 17): "Syzkaller FreeBSD support",
+    date(2016, 5, 27): "First KASAN mention in a fix",
+    date(2009, 10, 2): "User NULL mappings forbidden"
+}
+
+
+@dataclass
+class AdvisoryHistoryConfig(Config):
+    events: dict[date, str] = config_field(lambda: DEFAULT_EVENTS, help="Events to mark on the top X axis timeline")
 
 
 class CheriBSDAdvisoriesHistory(PlotTask):
@@ -19,13 +32,7 @@ class CheriBSDAdvisoriesHistory(PlotTask):
     public = True
     task_namespace = "kernel-advisories"
     task_name = "advisories-timeline"
-
-    events = {
-        date(2021, 4, 13): "Added KASAN kernel option",
-        date(2017, 10, 17): "Syzkaller FreeBSD support",
-        date(2016, 5, 27): "First KASAN mention in a fix",
-        date(2009, 10, 2): "User NULL mappings forbidden"
-    }
+    task_config_class = AdvisoryHistoryConfig
 
     @dependency
     def sa(self):
@@ -44,14 +51,14 @@ class CheriBSDAdvisoriesHistory(PlotTask):
         memory_safety_col = "Memory safety SA"
         spatial_safety_col = "Spatial memory safety SA"
 
-        view = df.group_by("Year").agg(pl.count().alias(total_col),
-                                       pl.col("kind_memory_safety").sum().alias(memory_safety_col),
-                                       (pl.col("kind_memory_safety") & ~pl.col("kind_uaf")
-                                        & ~pl.col("kind_race")).sum().alias(spatial_safety_col)).unpivot(
-                                            index=["Year"],
-                                            on=[total_col, memory_safety_col, spatial_safety_col],
-                                            variable_name="Category",
-                                            value_name="count")
+        cnt_memory_safety = pl.col("kind_memory_safety").sum().alias(memory_safety_col)
+        cnt_temporal_safety = (pl.col("kind_memory_safety") & ~pl.col("kind_uaf")
+                               & ~pl.col("kind_race")).sum().alias(spatial_safety_col)
+        agg_tmp = df.group_by("Year").agg(pl.count().alias(total_col), cnt_memory_safety, cnt_temporal_safety)
+        view = agg_tmp.unpivot(index=["Year"],
+                               on=[total_col, memory_safety_col, spatial_safety_col],
+                               variable_name="Category",
+                               value_name="count")
 
         with new_figure(self.timeline.paths()) as fig:
             ax = fig.subplots()
@@ -67,7 +74,7 @@ class CheriBSDAdvisoriesHistory(PlotTask):
                          style="Category")
             ax.set_ylabel("Number of advisories")
 
-            event_years = [d.year for d in self.events.keys()]
+            event_years = [d.year for d in self.config.events.keys()]
             for year in event_years:
                 ax.axvline(year, linestyle="-.", linewidth=1, color="gray")
 
@@ -76,24 +83,31 @@ class CheriBSDAdvisoriesHistory(PlotTask):
             ax_t.grid(visible=False)
             ax_t.set_xlim(*ax.get_xlim())
             ax_t.set_xticks(event_years)
-            ax_t.set_xticklabels(self.events.values(), rotation=-25, ha="right", size=8)
+            ax_t.set_xticklabels(self.config.events.values(), rotation=-25, ha="right", size=8)
 
-        # view = df.with_columns(
-        #     pl.col("date").str.to_datetime("%Y-%m-%d %H:%M:%S").dt.year().alias("Year")
-        # ).group_by("Year").agg(
-        #     pl.col("kind_memory_safety").sum().alias("memory_safety_advisories"),
-        #     pl.col("mitigation_temporal").sum().alias("temporal_safety"),
-        #     (pl.col("kind_memory_safety") & ~(pl.col("mitigation_unmitigated") | pl.col("mitigation_temporal"))).sum().alias("spatial_safety")
-        # )
 
-        # df["year"] = df.index.get_level_values("date").year
-        # df["advisory"] = df.index.get_level_values("advisory")
-        # # Some intermediate columns
-        # df["is_memory_safety"] = (df["not_memory_safety_bug"] == Marker.EMPTY)
-        # df["is_memory_safety_spatial"] = (df["is_memory_safety"] & ~df["uaf_uma_kmalloc"])
-        # df["is_mitigated_spatial"] = (df["cheri_does_not_help"] == Marker.EMPTY)
-        # df["is_mitigated_temporal"] = (df["cheri_does_not_help"] == Marker.TEMPORAL)
-        # df["is_mitigated"] = df["is_mitigated_spatial"] | df["is_mitigated_temporal"]
+class CheriBSDAdvisoriesPie(PlotTask):
+    """
+    Generate pie chart with different mitigation combinations.
+
+    Advisories mitigated by multiple CHERI features are accounted
+    separately.
+    """
+    public = True
+    task_namespace = "kernel-advisories"
+    task_name = "mitigation-pie-chart"
+
+    @dependency
+    def sa(self):
+        task = self.session.find_exec_task(IngestAdvisoriesTask)
+        return task.advisories.get_loader()
+
+    @output
+    def pie(self):
+        return PlotTarget(self, "mitigated-pie")
+
+    def run_plot(self):
+        pass
 
 
 class CheriBSDAdvisoriesTables(AnalysisTask):
@@ -101,71 +115,90 @@ class CheriBSDAdvisoriesTables(AnalysisTask):
     Generate summary tables CSV files from the main vulnerability analysis table.
     """
     public = True
-    task_namespace = "kernel-vuln"
+    task_namespace = "kernel-advisories"
     task_name = "summary-tables"
 
     @dependency
-    def sa_load(self):
-        return CheriBSDKernelVulnLoad(self.session, self.analysis_config)
+    def sa(self):
+        task = self.session.find_exec_task(IngestAdvisoriesTask)
+        return task.advisories.get_loader()
 
     @output
     def mitigation_summary(self):
-        return Target(self, "advisories-summary", ext="csv")
+        return Target(self, "mitigated", ext="csv")
 
     @output
     def unmitigated_summary(self):
-        return Target(self, "unmitigated-summary", ext="csv")
+        return Target(self, "unmitigated", ext="csv")
 
     def _advisories_summary(self):
-        df = self.sa_load.advisories_df.df.get()
+        df = self.sa.df.get()
 
-        all_memory = (df["not_memory_safety_bug"] == Marker.EMPTY).sum()
-        all_mitigated = ((df["cheri_does_not_help"] != Marker.YES) &
-                         (df["cheri_does_not_help"] != Marker.UNKNOWN)).sum()
+        selectors = [
+            pl.col("kind_memory_safety").sum().alias("all_memory_safety"),
+            (~pl.col("mitigation_unmitigated")).sum().alias("all_mitigated"),
+            # mitigated by pointer bounds but not involving sub-object bounds
+            (pl.col("mitigation_ptr_bounds") & ~pl.col("mitigation_subobject")).sum().alias("ptr_bounds_only"),
+            # mitigated by sub-object bounds
+            pl.col("mitigation_subobject").sum().alias("ptr_subobject"),
+            # mitigated by provenance or integrity
+            (pl.col("mitigation_ptr_provenance") | pl.col("mitigation_ptr_integrity")).sum().alias("ptr_integrity"),
+            # mitigated by temporal safety
+            pl.col("mitigation_temporal").sum().alias("ptr_temporal"),
+        ]
+        stats = df.select(selectors)
 
-        mitigation_df = pd.DataFrame({
-            "Description": [
+        table = {
+            "Category": [
+                "Mitigated by pointer provenance and integrity",
                 "Mitigated by bounds checking",
                 "Mitigated by sub-object bounds checking",
-                "Mitigated by pointer provenance and integrity",
                 "Total mitigated",
                 "Possibly mitigated by temporal safety",
                 "Total mitigated with temporal safety",
+            ],
+            "Number of advisories": [
+                stats["ptr_integrity"][0], stats["ptr_bounds_only"][0], stats["ptr_subobject"][0],
+                stats["all_mitigated"][0] - stats["ptr_temporal"][0], stats["ptr_temporal"][0],
+                stats["all_mitigated"][0]
             ]
-        })
-        mitigation_df["Number of advisories"] = [
-            (df["cheri_bounds_checking"] == Marker.YES).sum(), (df["cheri_subobject"] == Marker.YES).sum(),
-            ((df["cheri_ptr_integrity"] == Marker.YES) & (df["cheri_ptr_provenance"] == Marker.YES)).sum(),
-            (df["cheri_does_not_help"] == Marker.EMPTY).sum(), (df["cheri_temporal"] == Marker.TEMPORAL).sum(),
-            ((df["cheri_does_not_help"] == Marker.EMPTY) | (df["cheri_does_not_help"] == Marker.TEMPORAL)).sum()
-        ]
-        # Format % columns as string percentages
-        mitigation_df["% Of memory safety advisories"] = (mitigation_df["Number of advisories"] * 100 /
-                                                          all_memory).round(decimals=2)
-        mitigation_df["% Of advisories that can be mitigated"] = (mitigation_df["Number of advisories"] * 100 /
-                                                                  all_mitigated).round(decimals=2)
-        mitigation_df.to_csv(self.mitigation_summary.path, index=False, header=True)
+        }
+        table_df = pl.DataFrame(table).with_columns(
+            (pl.col("Number of advisories") * 100 /
+             stats["all_memory_safety"][0]).alias("% Of all memory safety advisories").round(2),
+            (pl.col("Number of advisories") * 100 /
+             stats["all_mitigated"][0]).alias("% Of mitigated advisories").round(2))
+
+        table_df.write_csv(self.mitigation_summary.single_path())
 
     def _unmitigated_summary(self):
-        advisory_df = self.sa_load.advisories_df.df.get()
-        df = self.sa_load.unmitigated_df.df.get()
+        df = self.sa.df.get()
 
-        all_unmitigated = ((advisory_df["not_memory_safety_bug"] == Marker.EMPTY) &
-                           (advisory_df["cheri_does_not_help"] == Marker.YES)).sum()
+        selectors = [
+            pl.col("kind_memory_safety").sum().alias("all_memory_safety"),
+            (pl.col("kind_memory_safety") & pl.col("mitigation_unmitigated")).sum().alias("all_unmitigated"),
+            (pl.col("unmitigated_category") == "uninit-memory").sum().alias("uninit_memory"),
+            (pl.col("unmitigated_category") == "stack-uaf").sum().alias("stack_uaf"),
+            (pl.col("unmitigated_category") == "vm-subsystem").sum().alias("vm"),
+            (pl.col("unmitigated_category") == "other").sum().alias("other")
+        ]
+        stats = df.select(selectors)
 
-        out_df = pd.DataFrame({
-            "Description": [
-                "Missing initialization of padding bytes", "Direct VM subsystem operations", "Stack temporal safety",
-                "Other"
-            ]
-        })
-        out_df["Number of advisories"] = [(df["reason_padding_initialization"] == Marker.YES).sum(),
-                                          (df["reason_direct_vm_subsystem_access"] == Marker.YES).sum(),
-                                          (df["reason_stack_use_after_free"] == Marker.YES).sum(),
-                                          (df["reason_other"] == Marker.YES).sum()]
-        out_df["% Of unmitigated advisories"] = (out_df["Number of advisories"] * 100 /
-                                                 all_unmitigated).round(decimals=2)
-        out_df.to_csv(self.unmitigated_summary.path, index=False, header=True)
+        renames = {
+            "uninit_memory": "Missing initialization of padding bytes",
+            "stack_uaf": "Stack temporal safety",
+            "vm": "Direct VM subsystem operations",
+            "other": "Other"
+        }
+        stats = stats.rename(renames)
+
+        table_df = stats.unpivot(on=list(renames.values()), variable_name="Category", value_name="Number of advisories")
+        table_df = table_df.with_columns(
+            (pl.col("Number of advisories") * 100 /
+             stats["all_memory_safety"][0]).alias("% Of memory safety advisories").round(2),
+            (pl.col("Number of advisories") * 100 /
+             stats["all_unmitigated"][0]).alias("% Of unmitigated advisories").round(2))
+        table_df.write_csv(self.unmitigated_summary.single_path())
 
     def run(self):
         self._advisories_summary()
