b4884d9e4aaa573120efb2ff3bcf2cfbbe64f8e0
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2025-11-14 16:48:28 +0000

    qps: Migrate QPS latency plot to new plot infrastructure.

--- pycheribenchplot/qps/qps_plot.py
+++ pycheribenchplot/qps/qps_plot.py
@@ -13,6 +13,24 @@ from ..core.task import dependency, output
 from .qps_exec import QpsExecTask
 
 
+class QpsPlotBase(SlicePlotTask):
+    """
+    Plot that operates on a slice of the QPS benchmark data.
+    """
+    @dependency
+    def qps_data(self):
+        for bench in self.slice_benchmarks:
+            task = bench.find_exec_task(QpsExecTask)
+            yield task.qps_driver_output.get_loader()
+
+    def _collect_data(self):
+        all_df = []
+        for loader in self.qps_data:
+            all_df.append(loader.df.get())
+        df = pl.concat(all_df, how="vertical", rechunk=True)
+        return df
+
+
 @dataclass
 class QpsPlotConfig(PlotGridConfig):
     """
@@ -29,9 +47,9 @@ class QpsPlotConfig(PlotGridConfig):
             raise ValidationError("One and only one of bar_plot or line_plot must be configured.")
 
 
-class QpsPlotTask(SlicePlotTask):
+class QpsPlotTask(QpsPlotBase):
     """
-    Plot the QPS throughput as a bar plot.
+    Plot the QPS throughput as a bar or line plot.
 
     The configuration can be used to select which metrics to include in the
     plot. The tiling for the metric kind is controlled via the `_metric_type`
@@ -42,12 +60,6 @@ class QpsPlotTask(SlicePlotTask):
     task_config_class = QpsPlotConfig
     public = True
 
-    @dependency
-    def qps_data(self):
-        for bench in self.slice_benchmarks:
-            task = bench.find_exec_task(QpsExecTask)
-            yield task.qps_driver_output.get_loader()
-
     @output
     def qps_plot(self):
         return PlotTarget(self, "qps")
@@ -55,13 +67,6 @@ class QpsPlotTask(SlicePlotTask):
     @output
     def qps_stats(self):
         return Target(self, "qps-stats", ext="csv")
-
-    def _collect_data(self):
-        all_df = []
-        for loader in self.qps_data:
-            all_df.append(loader.df.get())
-        df = pl.concat(all_df, how="vertical", rechunk=True)
-        return df
 
     def run_plot(self):
         df = self._collect_data()
@@ -91,5 +96,66 @@ class QpsPlotTask(SlicePlotTask):
                          x=plot_config.tile_xaxis,
                          y="qps",
                          err=["qps_low", "qps_high"],
+                         config=plot_config)
+            grid.add_legend()
+
+
+class QpsLatencyPlotTask(QpsPlotBase):
+    """
+    Plot the QPS latency as a bar or line plot.
+
+    The QPS output included the full histogram of the recorded latencies along with
+    the pre-computed latency percentiles.
+    This produces the median 90th percentile latency over the benchmark iterations.
+    NOTE: This is _NOT_ the bootstrapped 90th percentile latency. It is an indication
+    of the central tendency of the 90th percentile statistic.
+    """
+    task_namespace = "qps"
+    task_name = "latency-plot"
+    task_config_class = QpsPlotConfig
+    public = True
+
+    @output
+    def latency_plot(self):
+        return PlotTarget(self, "latency")
+
+    @output
+    def latency_stats(self):
+        return Target(self, "latency-stats", ext="csv")
+
+    def run_plot(self):
+        df = self._collect_data()
+        # usec to msec
+        df = df.with_columns(
+            pl.col("latency50") / 1000,
+            pl.col("latency90") / 1000,
+            pl.col("latency95") / 1000,
+            pl.col("latency99") / 1000)
+
+        self.logger.info("Compute QPS latency statistics")
+        stats = self.compute_overhead(df, "latency90", how="median", overhead_scale=100)
+
+        if self.config.drop_relative_baseline:
+            view_df = stats.filter((pl.col("_metric_type") == "absolute") | ~pl.col("_is_baseline"))
+        else:
+            view_df = stats
+
+        self.logger.info("Generate QPS plot")
+        with PlotGrid(self.latency_plot, view_df, self.config) as grid:
+            # Dump the sorted tabular data using the ordering specified by the grid config
+            dump_df = grid.get_grid_df()
+            dump_df.write_csv(self.latency_stats.single_path())
+
+            if plot_config := self.config.bar_plot:
+                grid.map(grid_barplot,
+                         x=plot_config.tile_xaxis,
+                         y="latency90",
+                         err=["latency90_low", "latency90_high"],
+                         config=plot_config)
+            if plot_config := self.config.line_plot:
+                grid.map(grid_lineplot,
+                         x=plot_config.tile_xaxis,
+                         y="latency90",
+                         err=["latency90_low", "latency90_high"],
                          config=plot_config)
             grid.add_legend()
