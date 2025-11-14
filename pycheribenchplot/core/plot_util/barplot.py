3dc309071672ff32a2c313608f09f95f3afd5982
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2025-11-14 14:25:14 +0000

    plot: Further cleanup of barplot implementation.

--- pycheribenchplot/core/plot_util/barplot.py
+++ pycheribenchplot/core/plot_util/barplot.py
@@ -53,7 +53,6 @@ def grid_barplot(tile: "PlotTile",
     assert not y.startswith("<")
     assert tile.hue is not None
     assert not tile.hue.startswith("<")
-    palette = tile.palette
 
     if config.orient == "x" and not chunk[y].dtype.is_numeric():
         raise TypeError("Y axis values must be numeric when orient='x'")
@@ -79,10 +78,13 @@ def grid_barplot(tile: "PlotTile",
     # XXX handle stacked + shifted
     coordgen_kwargs = {}
     if config.stack_by:
-        cgen_config = CoordGenConfig(stack_by=tile.hue, **coordgen_kwargs)
+        coordgen_kwargs["stack_by"] = tile.hue
     elif config.shift_by:
-        cgen_config = CoordGenConfig(shift_by=tile.hue, **coordgen_kwargs)
+        coordgen_kwargs["shift_by"] = tile.hue
+    else:
+        coordgen_kwargs["shift_by"] = tile.hue
 
+    cgen_config = CoordGenConfig(**coordgen_kwargs)
     view = cgen.compute_coordinates(chunk, independent_var=i_var, dependent_vars=[d_var], config=cgen_config)
 
     # Assign categorical axis ticks and labels
@@ -94,12 +96,12 @@ def grid_barplot(tile: "PlotTile",
         error_kwargs = {}
         if err:
             lower, upper = err
-            err_low = (hue_group[metric] - hue_group[lower]).abs()
-            err_high = (hue_group[upper] - hue_group[metric]).abs()
+            err_low = (hue_group[d_var] - hue_group[lower]).abs()
+            err_high = (hue_group[upper] - hue_group[d_var]).abs()
             error_kwargs.update({f"{orthogonal_orient}err": (err_low, err_high), "capsize": 4})
         coord = hue_group["__gen_coord"] + hue_group[f"__gen_offset"]
         bar_plot(coord,
-                 hue_group[metric],
+                 hue_group[d_var],
                  hue_group["__gen_width"],
                  hue_group["__gen_stack"],
                  color=color,
