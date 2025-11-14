3883013304cbcfaaac3c83d7de663c91a2b45f01
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2025-11-14 17:56:44 +0000

    plot: Migrate lineplot to new ColRef infrastructure.

--- pycheribenchplot/core/plot_util/lineplot.py
+++ pycheribenchplot/core/plot_util/lineplot.py
@@ -6,7 +6,7 @@ import numpy as np
 import polars as pl
 
 from ..config import Config, config_field
-from .plot_grid import PlotConfigBase, PlotTile
+from .plot_grid import ColRef, PlotConfigBase, PlotTile
 
 
 class Scale(Enum):
@@ -20,14 +20,14 @@ class LinePlotConfig(PlotConfigBase):
     """
     Display grid configuration extension specific to line plots.
     """
-    tile_xaxis: str = config_field(Config.REQUIRED, desc="Parameter to use for the X axis of each tile.")
+    tile_xaxis: ColRef = config_field(Config.REQUIRED, desc="ColRef to use for the X axis of each tile.")
     tile_xscale: Scale = config_field(Scale.Linear, desc="Scale for the X axis.")
     tile_yscale: Scale = config_field(Scale.Linear, desc="Scale for the Y axis.")
     line_width: float | None = config_field(None, desc="Width of the lines.")
     marker_fill: bool = config_field(True, desc="When false, only draw the marker outline.")
     marker_size: float | None = config_field(None, desc="Size of the markers.")
-    hue_marker: dict[str, str] = config_field(dict, desc="Set the marker style for each hue.")
-    hue_linestyle: dict[str, str] = config_field(dict, desc="Set the line style for each hue.")
+    marker: ColRef | None = config_field(None, desc="ColRef containing the markers. Should align on the hue.")
+    linestyle: ColRef | None = config_field(None, desc="ColRef containing the linestyles. Should align on the hue.")
 
     def uses_param(self, name: str) -> bool:
         return (super().uses_param(name) or self.tile_xaxis == name)
@@ -49,30 +49,21 @@ def generate_x_coords(df: pl.DataFrame, x: str) -> pl.DataFrame:
 
 def grid_lineplot(tile: PlotTile,
                   chunk: pl.DataFrame,
+                  config: LinePlotConfig,
                   x: str,
                   y: str,
-                  err: tuple[str, str] | None = None,
-                  config: LinePlotConfig | None = None):
+                  err: tuple[str, str] | None = None):
     """
     Create a line plot with error bars.
     """
-    try:
-        # Handle column renaming if we have a DisplayTile
-        x = tile.d[x]
-        y = tile.d[y]
-        hue = tile.d[tile.hue]
-    except AttributeError:
-        hue = tile.hue
-
-    # XXX this is a common operation and should probably be part of the PlotGrid
-    if hue is None:
-        # Create a fake hue column with a non-null value
-        chunk = chunk.with_columns(pl.lit("__no_hue__").alias("__gen_hue"))
-        hue = "__gen_hue"
-        tile.palette = plt.rcParams["axes.prop_cycle"].by_key()["color"][:1]
+    # Note: here all the column refs should have been resolved
+    x = tile.ref_to_col(x)
+    assert not y.startswith("<")
+    assert tile.hue is not None
+    assert not tile.hue.startswith("<")
 
     if chunk[x].dtype.is_numeric():
-        view = chunk.with_columns(pl.col(x).alias("__gen_coord"))
+        view = chunk.with_columns(pl.col(x).alias("_x"))
     else:
         # Use coord allocator to produce coordinates or this is
         # just a very simple case in which we don't have to stack/shift?
@@ -94,26 +85,25 @@ def grid_lineplot(tile: PlotTile,
         case Scale.Log10:
             tile.ax.set_yscale("log", base=10)
 
-    for (hue_label, ), hue_group in view.group_by(hue, maintain_order=True):
+    # Add knob to force xticks vs custom xticks vs automatic?
+    x_vals = view["_x"].unique(maintain_order=True)
+    tile.ax.set_xticks(x_vals)
+    tile.ax.set_xlim(x_vals.min(), x_vals.max())
+    tile.ax.set_xlabel(x)
+    tile.ax.set_ylabel(y)
+
+    for (hue_label, ), hue_group in view.group_by(tile.hue, maintain_order=True):
         color = tile.palette[hue_label]
         plot_kwargs = config.lineplot_kwargs()
-        # XXX this is broken by renaming, there should be a better way to do this, perhaps
-        # via the base plot config
-        if marker := config.hue_marker.get(hue_label):
-            plot_kwargs["marker"] = marker
-        if linestyle := config.hue_linestyle.get(hue_label):
-            plot_kwargs["linestyle"] = linestyle
+        if marker_ref := config.marker:
+            plot_kwargs["marker"] = hue_group[tile.ref_to_col(marker_ref)].first()
+        if line_ref := config.linestyle:
+            plot_kwargs["linestyle"] = hue_group[tile.ref_to_col(line_ref)].first()
         if err:
             lower, upper = err
             err_low = (hue_group[y] - hue_group[lower]).abs()
             err_high = (hue_group[upper] - hue_group[y]).abs()
             plot_kwargs.update({"yerr": (err_low, err_high), "capsize": 4})
-            tile.ax.errorbar(hue_group["__gen_coord"], hue_group[y], color=color, label=hue_label, **plot_kwargs)
+            tile.ax.errorbar(hue_group["_x"], hue_group[y], color=color, label=hue_label, **plot_kwargs)
         else:
-            tile.ax.plot(hue_group["__gen_coord"], hue_group[y], color=color, label=hue_label, **plot_kwargs)
-        xval = hue_group["__gen_coord"]
-        tile.ax.set_xticks(xval)
-        tile.ax.set_xlim(xval.min(), xval.max())
-
-    tile.ax.set_ylabel(y)
-    tile.ax.set_xlabel(x)
+            tile.ax.plot(hue_group["_x"], hue_group[y], color=color, label=hue_label, **plot_kwargs)
