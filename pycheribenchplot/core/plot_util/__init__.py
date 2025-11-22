d2d28803fbf1e70d82c1115206a284355d35715a
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2025-11-22 12:07:08 +0000

    Re-introduce pointplot import.

--- pycheribenchplot/core/plot_util/__init__.py
+++ pycheribenchplot/core/plot_util/__init__.py
@@ -1,5 +1,14 @@
 from .barplot import BarPlotConfig, grid_barplot
 from .lineplot import LinePlotConfig, grid_lineplot
 from .plot_grid import PlotGrid, PlotGridConfig
+from .pointplot import grid_pointplot
 
-__all__ = (BarPlotConfig, LinePlotConfig, PlotGrid, PlotGridConfig, grid_barplot, grid_lineplot)
+__all__ = (
+    BarPlotConfig,
+    LinePlotConfig,
+    PlotGrid,
+    PlotGridConfig,
+    grid_barplot,
+    grid_lineplot,
+    grid_pointplot,
+)
