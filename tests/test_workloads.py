9bad9faf828c68a79153b691c943939519db9819
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2026-05-30 16:37:07 +0100

    test: Add automated testing for all workload configurations.
    This is designed to maintain the workload configurations so that they
    remain in sync with the tooling.

---
+++ tests/test_workloads.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import shutil
+
+import polars as pl
+import pytest
+
+from pycheribenchplot.core.config import BenchplotUserConfig, PipelineConfig
+from pycheribenchplot.core.session import Session
+
+
+ASSETS_DIR = Path(__file__).parent.parent / "workloads"
+
+# Note: rely on the convention that analysis configurations will be named foo.analysis.json
+config_files = [
+    c for c in ASSETS_DIR.rglob("*.json") if not c.name.endswith(".analysis.json")
+]
+
+
+def generate_id(path):
+    return path.name
+
+
+@pytest.fixture
+def session_path(tmp_path):
+    shutil.rmtree(tmp_path)
+    return tmp_path
+
+
+@pytest.mark.parametrize("config_file", config_files, ids=generate_id)
+def test_configuration(config_file, session_path):
+    """
+    Attempt to parse every workload configuration an initialize the
+    corresponding session.
+    This ensures that all workloads configurations are properly maintained.
+    """
+    user_config = BenchplotUserConfig()
+    workload = PipelineConfig.load_json(config_file)
+    session = Session.make_new(user_config, workload, session_path)
+    session.generate()
+
+    # TODO Generate mock data for analysis tests
+
+    session.clean_all()
